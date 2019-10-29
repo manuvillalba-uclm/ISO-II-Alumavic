5e631e170a75eb445a4b7d80c84916115e4bfe9e
Manu <56063961+manuvillalba-uclm@users.noreply.github.com>
2019-10-29 19:22:51 +0100

    Add files via upload

---
+++ src/main/java/ISOII/alumavic/tallercoches/App.java
@@ -0,0 +1,13 @@
+package ISOII.alumavic.tallercoches;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
